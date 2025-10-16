6fddb65e63b10a6be4568c843db1bd75ffeb6a6c
Nima Kelidari <Nikelroid@gmail.com>
2025-10-16 00:00:23 -0700

    change params

--- curriculum_manager.py
+++ curriculum_manager.py
@@ -64,9 +64,9 @@ class CurriculumManager:
     
     def _get_current_phase(self) -> int:
         """Determine current training phase"""
-        if self.total_steps < 10_000:
+        if self.total_steps < 1_000:
             return 1
-        elif self.total_steps < 20_000:
+        elif self.total_steps < 2_000:
             return 2
         else:
             return 3
